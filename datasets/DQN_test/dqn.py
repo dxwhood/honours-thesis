60525faf641994fdaad64cd8a7ffcb2839608fc5
dxwhood <damien.hood@hotmail.com>
2023-10-11 18:47:25 -0400

    update

--- datasets/DQN_test/dqn.py
+++ datasets/DQN_test/dqn.py
@@ -4,7 +4,11 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-print("test")
-#print the python version
-import sys
-print(sys.version)
+class DQN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(DQN, self).__init__()
+        self.fc = nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.fc(x)
+    
