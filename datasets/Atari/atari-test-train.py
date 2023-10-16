10243c7465fe264e731d24a911ca0b0694de43e9
dxwhood <damien.hood@hotmail.com>
2023-10-15 21:45:25 -0400

    test

---
+++ datasets/Atari/atari-test-train.py
@@ -0,0 +1,23 @@
+from stable_baselines3 import PPO
+import gymnasium as gym  # Using gymnasium instead of gym
+
+
+# Initialize the environment
+env = gym.make('ALE/Pong-v5')
+
+# Define hyperparameters
+model = PPO(
+    "CnnPolicy", 
+    env, 
+    verbose=1,
+    n_steps=2048,
+    ent_coef=0.01,
+    learning_rate=0.00025,
+    clip_range=0.1
+)
+
+# Train the model
+model.learn(total_timesteps=100000)
+
+# Save the model
+model.save("ppo_pong")
